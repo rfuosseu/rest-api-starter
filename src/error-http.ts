b615db8c68969b1d4f958a9d120aebd584fedbd7
Rodrigue FUOSSEU <fnfrodrigue@gmail.com>
2021-04-08 19:11:38 +0200

    feature - api request (wip)

--- src/error-http.ts
+++ src/error-http.ts
@@ -1,7 +1,8 @@
-export default class ErrorHttp {
+export default class ErrorHttp extends Error {
   messages: string[];
 
   constructor(public status: number, msg: string | string[]) {
+    super(msg.toString());
     this.status = this.status || 500;
     this.messages = Array.isArray(msg) ? msg : [msg];
   }
